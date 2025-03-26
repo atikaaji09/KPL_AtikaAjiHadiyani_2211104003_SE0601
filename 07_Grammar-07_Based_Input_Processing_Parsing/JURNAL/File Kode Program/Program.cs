951e0c5907265d3621c6f9f70350552602950453
Atika Aji Hadiyani <atikaajiii@gmail.com>
2025-03-26 15:05:41 +0700

    Menambahkan file program

---
+++ 07_Grammar-07_Based_Input_Processing_Parsing/JURNAL/File Kode Program/Program.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        //DataMahasiswa2211104003.ReadJSON();
+        //TeamMembers2211104003.ReadJSON();
+        GlossaryItem2211104003.ReadJSON();
+    }
+}
+
