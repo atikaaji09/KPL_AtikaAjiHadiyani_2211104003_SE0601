5323b75daca09b2a266b9d3d643f578fc055c4ba
Atika Aji Hadiyani <atikaajiii@gmail.com>
2025-04-18 00:40:30 +0700

    Menambahkan hasil praktikum modul 8

---
+++ 08_Runtime_Configuration_dan_Internationalization/GUIDED/File Program/Services/LanguageService.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace GuidedModul8.Services
+{
+    public class SharedResource
+    {
+    }
+
+    public class LanguageService
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public LanguageService(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _localizer = factory.Create("SharedResource", assemblyName.Name);
+        }
+
+        public LocalizedString Getkey(string key)
+        {
+            return _localizer[key];
+        }
+    }
+}
