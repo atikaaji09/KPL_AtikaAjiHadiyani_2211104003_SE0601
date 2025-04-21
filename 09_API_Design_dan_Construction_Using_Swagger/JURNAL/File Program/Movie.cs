9309077639e15051120d04e6ba67a8d1d661f81a
Atika Aji Hadiyani <atikaajiii@gmail.com>
2025-04-21 13:23:57 +0700

    Feat: menambahkan API Movies

---
+++ 09_API_Design_dan_Construction_Using_Swagger/JURNAL/File Program/Movie.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace modul9_2211104003
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie() { }
+    }
+}
+
